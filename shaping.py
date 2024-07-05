d4967c1feb7067da5beacd7ff1a194e878f5035a
Abdul Haseeb MP <haseebhaseebmp@gmail.com>
2024-07-05 11:30:51 +0530

    shaping , index, numpy arange function ,ones zeros, spliting through linspace

---
+++ shaping.py
@@ -0,0 +1,40 @@
+import numpy as np
+#reshaping , index
+haseeb=ar9.reshape(3,5)
+haseeb
+haseeb[0]
+haseeb[0,4]
+haseeb[:,4]
+haseeb[1,4]
+haseeb[0:1,2]
+haseeb[1:2,2]
+haseeb[0:2,1]
+haseeb[1:,3]
+haseeb[1:,0]
+haseeb[1:,4]
+haseeb[1:3,0:5:4]
+#numpy arrange function
+list(range(1,18))
+np.arange(0,17,step=2)
+#ones zeros
+np.ones(3)
+np.ones([3,3,3])
+np.zeros([10,10,5])
+np.zeros([5,2,5])
+#spliting linspace
+np.linspace(0,100,5)
+np.linspace(0,1000,5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
